aee1759b7fe6eaa2a4e7627086e4c43a55520511
MadhuriKadam45 <Madhurikadam45@proton.me>
2024-10-06 09:03:58 +0000

    render pokemon name

--- README.md
+++ README.md
@@ -9,4 +9,13 @@ add to run: mnpm start and not using live-server
 
 import bootstrap.css
 navbar with icon8 image
-cards with 4 col lg, 3 col md, 1 col
+cards with 4 col lg, 3 col md, 1 col on smaller
+use of class: display-1, my-3
+
+---
+
+link js file
+we made the input element focus on / key press
+we have to import data.json inside main.js
+`import data from 'data.json';`
+add <script>
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,29 @@
+import data from "./data.json";
+
+// DOM Targeting
+const cardsRow = document.querySelector("#cards-row");
+const inpurEl = document.querySelector("input");
+
+for (let pokemonObj of data) {
+  const p = document.createElement("p");
+  p.textContent = pokemonObj.name;
+  cardsRow.appendChild(p);
+}
+
+console.log(cardsRow);
+
+for (let pokemonObj of data) {
+  console.log(pokemonObj.name);
+}
+
+// Target: to log all the data
+
 // To target the input element
 const inputEl = document.querySelector("input");
 // Log the targetted element
 // inputEl.focus();
 
+// focus input on / keypress
 document.addEventListener("keypress", function (event) {
   if (event.key === "/") {
     // Don't input (/)
